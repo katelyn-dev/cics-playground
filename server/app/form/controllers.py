0968a114cc02d860ea6fb96f17ddf9031bc17a12
kyxfox <102643024+Kin-ck@users.noreply.github.com>
2024-08-01 22:44:06 -0400

    placeholders for form API

--- server/app/form/controllers.py
+++ server/app/form/controllers.py
@@ -3,6 +3,14 @@ from flask import Blueprint
 
 mod = Blueprint('form', __name__)
 
+@mod.route("/createForm")
+def create_form():
+  return "create form"
+
 @mod.route("/submitForm")
 def submit_form():
   return "Hello World"
+
+@mod.route("/emailNotification")
+def email_notification():
+  return "email notification"

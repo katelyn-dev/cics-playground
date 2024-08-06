1553c719f88f45f34f56a62c619465ce724f6f5a
Kin Yung <102643024+Kin-ck@users.noreply.github.com>
2024-08-05 23:16:50 -0400

    form model update and files restructure

--- server/app/form/controllers.py
+++ server/app/form/controllers.py
@@ -1,20 +1,21 @@
 from flask import Blueprint, Flask, request, jsonify, make_response
 from .models import Form, form_schema, forms_schema
 from app import db, ma
-from app.email.EmailController import EmailController
+from .email_controller import EmailController
 from app.config import Config
 from datetime import datetime
+from sqlalchemy import and_
 
 mod = Blueprint('form', __name__)
 
 """
     Expected JSON payload:
     {
-        "toEmail": "example@example.com",      # Required. The recipient's email address.
-        "toName": "Recipient Name",            # Required. The recipient's name.
-        "registrationDetail": "Details here",  # Required. Registration details.
-        "registrationFee": "100",              # Required. Registration fee amount.
-        "paymentLink": "http://example.com"    # Required. Payment link.
+        "form_json": "{"First Name": "Text",....},
+        "created_by": "Kin",
+        "form_prefix": "Test"
+        "last_updated_by": "Kin",
+        "start_time": "2024-08-01"
     }
 """
 @mod.route("/createForm", methods=['POST'])
@@ -25,13 +26,36 @@ def create_form():
     return jsonify({"error": "Invalid data format, expected a dictionary"}), 400
   
   form_model = Form()
-  form_model.create_form(data)
+  form_id = form_model.create_form(data)
 
-  return jsonify({"message": "Form created successfully"}), 201
+  return jsonify({"message": f"Form created successfully with form_id: {form_id}"}), 201
 
 
 @mod.route("/submitForm", methods=['POST'])
 def submit_form():
+
+  data = request.get_json()
+
+  # Mapping of fields to tables
+  class_fields = {
+      'class_group_id', 'class_name_eng', 'class_name_zhcn', 'class_name_zhhk',
+      'has_subclass', 'subclass_group_id', 'has_extra_attributes', 'extra_attributes_name',
+      'extra_attributes', 'class_start', 'class_end', 'start_time', 'last_modified_time'
+  }
+  contact_fields = {
+      'application_id', 'student_id', 'guardian_full_name', 'guardian_relationship',
+      'emergency_contact_name', 'emergency_relationship', 'emergency_phone'
+  }
+  student_fields = {
+      'lastname', 'firstname', 'gender', 'phone_number', 'email', 'identity_status',
+      'date_of_birth', 'age', 'age_group', 'address_street', 'address_city',
+      'address_postal_code', 'country_of_origin', 'length_of_residence', 'is_first_time_apply',
+      'is_followed_ig', 'is_signed', 'is_consent', 'is_paid', 'comment', 'start_time',
+      'last_modified_time'
+  }
+
+
+
   return "Form submited"
 
 
@@ -52,7 +76,7 @@ def email_notification():
     return jsonify({"error": "Invalid data format, expected a dictionary"}), 400
   
   email_controller = EmailController(Config.EMAILJS_SERVICE_ID, Config.EMAILJS_USERID, Config.EMAILJS_FROM_NAME, Config.EMAILJS_ACCESS_TOKEN, Config.EMAILJS_TEMPLATE_ID)
-  email_controller.send_email(data.get('toEmail'),data.get('toName'),data.get('registrationDetail'), data.get('registrationFee'), data.get('paymentLink'))
+  email_controller.send_email(data.get('to_email'),data.get('to_name'),data.get('registration_detail'), data.get('registration_fee'), data.get('payment_link'))
 
   return "Email sent"
 
@@ -77,30 +101,25 @@ def get_all_form():
 # http://127.0.0.1:8080/searchForm?startDate=yyyy-mm-dd&endDate=yyyy-mm-dd
 """
     Query parameters:
-    - startDate (required): The start date for the search in YYYY-MM-DD format.
-    - endDate (required): The end date for the search in YYYY-MM-DD format.
+    - startDate (optional): The start date for the search in YYYY-MM-DD format.
+    - endDate (optional): The end date for the search in YYYY-MM-DD format.
+    - formID (optional): The form ID to search
+    - isActive (optional): Specific the form status to search
 """
 @mod.route("/searchForm", methods=["GET"])
 def searchForm():
-    start_date_str = request.args.get('startDate')
-    end_date_str = request.args.get('endDate')
 
-    if start_date_str and end_date_str:
-        # Convert the string parameters to date objects
-        try:
-            start_date = datetime.strptime(start_date_str, "%Y-%m-%d").date()
-            end_date = datetime.strptime(end_date_str, "%Y-%m-%d").date()
-        except ValueError:
-            return jsonify({"error": "Invalid date format"}), 400
-        
-        # Query the database for forms within the specified date range
-        forms = Form.query.filter(
-            Form.start_time >= start_date,
-            Form.start_time <= end_date
-        ).all()
-        
+    query_params = request.args
+    try:
+        filters = Form.apply_filters(query_params)
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+
+    if filters:
+        # Apply filters to query
+        forms = Form.query.filter(*filters).all()
         if forms:
-          return make_response(forms_schema.dump(forms), 200)
+            return make_response(forms_schema.dump(forms), 200)
         return make_response(jsonify({'message': 'form not found'}), 404)
     else:
-        return jsonify({"error": "Missing startTime or endTime parameter"}), 400
+        return jsonify({"error": "Missing query parameters"}), 400
--- server/app/form/models.py
+++ server/app/form/models.py
@@ -1,20 +1,40 @@
 from app import db, ma
 from sqlalchemy.dialects.mysql import JSON
+from sqlalchemy import and_
+from datetime import datetime
 
 class Form(db.Model):
     __tablename__ = 'form'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    form_json = db.Column(db.Text(255))
+    form_id = db.Column(db.String(255))
+    form_json = db.Column(db.Text)
     created_by = db.Column(db.String(255))
     last_updated_by = db.Column(db.String(255))
+    is_active = db.Column(db.Boolean)
     start_time = db.Column(db.DateTime)
     last_modified_time = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+    
+    @classmethod
+    def create_form(cls, data):
+        
+        # Get the largest form ID with form_prefix
+        form_prefix = data.get('form_prefix')
+        max_form_id = db.session.query(
+            db.func.max(db.cast(db.func.substring(cls.form_id, len(form_prefix) + 2), db.Integer))
+        ).filter(cls.form_id.like(f"{form_prefix}_%")).scalar()
 
-    def create_form(self, data):
+        # Determine the new form number
+        new_form_number = 1 if max_form_id is None else max_form_id + 1
+
+        # Generate the new form_id
+        new_form_id = f"{form_prefix}_{new_form_number:05d}"
+
         # Create a new Form instance
         new_form = Form(
+            form_id=new_form_id,
             form_json=str(data.get('form_json')),
+            is_active = True,
             created_by=data.get('created_by'),
             last_updated_by=data.get('last_updated_by'),
             start_time=data.get('start_time')
@@ -24,16 +44,120 @@ class Form(db.Model):
         db.session.add(new_form)
         db.session.commit()
 
+        return new_form_id
+    
+    @classmethod
+    def apply_filters(cls, query_params):
+        filters = []
+
+        # Check for formID and add to filters if present
+        form_id = query_params.get('formID')
+        if form_id:
+            filters.append(cls.form_id == form_id)
+
+        # Check for startDate and endDate and add to filters if present
+        start_date_str = query_params.get('startDate')
+        end_date_str = query_params.get('endDate')
+        if start_date_str and end_date_str:
+            try:
+                start_date = datetime.strptime(start_date_str, "%Y-%m-%d").date()
+                end_date = datetime.strptime(end_date_str, "%Y-%m-%d").date()
+                filters.append(and_(cls.start_time >= start_date, cls.start_time <= end_date))
+            except ValueError:
+                raise ValueError("Invalid date format")
+
+        # Check for isActive and add to filters if present
+        is_active = query_params.get('isActive')
+        if is_active is not None:
+            filters.append(cls.is_active == (is_active.lower() == 'true'))
+
+        return filters
+
     def __repr__(self):
         rep = 'Form(' + str(self.class_group_id) + ',' + self.class_name_eng + ')'
         return rep
     
-        
 class FormSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Form
         load_instance = True
         sqla_session = db.session
+
+"""
+class EmergencyContact(db.Model):
+    __tablename__ = 'emergency_contacts'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    application_id = db.Column(db.Integer, nullable=False)
+    student_id = db.Column(db.Integer, nullable=False)
+    guardian_full_name = db.Column(db.String(255))
+    guardian_relationship = db.Column(db.String(255))
+    emergency_contact_name = db.Column(db.String(255))
+    emergency_relationship = db.Column(db.String(255))
+    emergency_phone = db.Column(db.String(255))
+
+    @classmethod
+    def add_emergency_contact(cls, data):
+        contact_data = {key: value for key, value in data.items() if hasattr(cls, key)}
+        
+        new_contact = cls(**contact_data)
+        db.session.add(new_contact)
+        db.session.commit()
+
+        return new_contact.id
+
+class Student(db.Model):
+    __tablename__ = 'students'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    lastname = db.Column(db.String(255), nullable=False)
+    firstname = db.Column(db.String(255))
+    gender = db.Column(db.String(10))
+    phone_number = db.Column(db.String(20))
+    email = db.Column(db.String(255))
+    identity_status = db.Column(db.String(255))
+    date_of_birth = db.Column(db.Date)
+    age = db.Column(db.Integer)
+    age_group = db.Column(db.String(255))
+    address_street = db.Column(db.String(255))
+    address_city = db.Column(db.String(255))
+    address_postal_code = db.Column(db.String(255))
+    country_of_origin = db.Column(db.String(255))
+    length_of_residence = db.Column(db.String(255))
+    is_first_time_apply = db.Column(db.String(1))
+    is_followed_ig = db.Column(db.String(1))
+    is_signed = db.Column(db.String(1))
+    is_consent = db.Column(db.String(1))
+    is_paid = db.Column(db.String(1))
+    comment = db.Column(db.String(255))
+    start_time = db.Column(db.DateTime)
+    last_modified_time = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+
+    @classmethod
+    def add_student(cls, data):
+        student_data = {key: value for key, value in data.items() if hasattr(cls, key)}
+        
+        new_student = cls(**student_data)
+        db.session.add(new_student)
+        db.session.commit()
+
+        return new_student.id
+    
+    @classmethod
+    def apply_filters(cls, query_params):
+        filters = []
+
+        email_str = query_params.get('email')
+        if email_str:
+            filters.append(cls.email == email_str)
+
+        firstname_str = query_params.get('firstname')
+        lastname_str = query_params.get('lastname')
+        if firstname_str and lastname_str:
+            filters.append(and_(cls.firstname == firstname_str , cls.lastname == lastname_str))
+
+        return filters
+"""
 
 form_schema = FormSchema()
 forms_schema = FormSchema(many=True)

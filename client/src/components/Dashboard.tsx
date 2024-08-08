4396e8bf51e706408f9e7c0765c090be8a85db08
Jimmynpt <jimmyng@jimmydembp.home>
2024-08-08 19:39:47 -0400

    dashboard

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,62 +1,195 @@
 "use client";
-import React, {ChangeEvent, FormEvent, useEffect, useRef, useState} from 'react';
+import React, { useEffect, useState, FormEvent, ChangeEvent } from "react";
+import axios from "axios";
 import styles from "../styles/Dashboard.module.css";
+import { NavLink } from "react-router-dom";
 
-type classes={
-  classes:{
-    id :string
-    class_group_id :string,
-    class_name_eng :string,
-    class_name_zhcn :string,
-    class_name_zhhk :string,
-    has_subclass :string,
-    subclass_group_id :string,
-    has_extra_attributes :string,
-    extra_attributes_name :string,
-    extra_attributes :JSON,
-    class_start :Date,
-    class_end :Date,
-    start_time :string,
-    last_modified_time :string,
-    age_group :string,
-  }
-    
+interface TimeSlot {
+  startDate: string;
+  endDate: string;
 }
 
-export const Dashboard: React.FunctionComponent<classes> =(classes)=> {
-  console.log(classes)
+export interface TimeSlot_data {
+  start_date: Date;
+  end_date: Date;
+}
+interface SelectedProgrammeData {
+  searchWord: string;
+  selectedProgramme: DisplayProgrammeData;
+  expectedStartDate: string;
+  expectedEndDate: string;
+  displayedProgrammes: DisplayProgrammeData[];
+}
+interface DisplayProgrammeData {
+  id: string;
+  class_name_eng: string;
+  class_name_zhcn: string;
+  class_name_zhhk: string;
+  target_audience: string;
+  class_group_id: string;
+  start_time: string;
+  class_end: string;
+}
+
+const Dashboard = () => {
+  const [totalStudents, setTotalStudents] = useState(0);
+  const [totalCourses, setTotalCourses] = useState(0);
+  const [totalForms, setTotalForms] = useState(0);
+
+  const [selectedProgrammeData, setSelectedProgrammeData] =
+    useState<SelectedProgrammeData>({
+      searchWord: "",
+      selectedProgramme: {
+        id: "",
+        class_name_eng: "",
+        class_name_zhcn: "",
+        class_name_zhhk: "",
+        target_audience: "",
+        class_group_id: "",
+        start_time: "",
+        class_end: "",
+      },
+      expectedStartDate: "",
+      expectedEndDate: "",
+      displayedProgrammes: [],
+    });
+
+  const [timeslot, setTimeslot] = useState<TimeSlot>({
+    startDate: "",
+    endDate: "",
+  });
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://127.0.0.1:8080/dashboard");
+        const { totalStudents, totalCourses, totalForms } = response.data;
+        setTotalStudents(totalStudents);
+        setTotalCourses(totalCourses);
+        setTotalForms(totalForms);
+      } catch (error) {
+        console.error("Error fetching dashboard data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value, type, checked } = e.target;
+    setTimeslot((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    await searchStudents();
+    // console.log(timeslot.startDate);
+  };
+
+  const searchStudents = async () => {
+    const { startDate, endDate } = timeslot;
+    const searchProgrammeUrl = `${process.env.REACT_APP_BASE_URL}/searchProgramme?startDate=${startDate}&endDate=${endDate}`;
+    try {
+      const response = await axios.get<DisplayProgrammeData[]>(searchProgrammeUrl);
+      const displayList = response.data;
+      setSelectedProgrammeData((prev) => ({
+        ...prev,
+        displayedProgrammes: displayList,
+      }));
+    } catch (error) {
+      console.log(error);
+      setSelectedProgrammeData((prev) => ({
+        ...prev,
+        displayedProgrammes: [],
+      }));
+    }
+  };
+
+  const renderDashboard = () => {
     return (
-      <main className={styles.container}>
-        <div className={styles.card}>
-          <h2>Total Courses</h2>
-          <p className={styles.card_words}>Loading...</p>
-          <link href="/programme" className={styles.btn}>Manage Courses</link>
-        </div>
-        <div className={styles.card}>
-          <h2>Registered Students</h2>
-          <p className={styles.card_words}>Loading...</p>
-          <link href="/students" className={styles.btn}>View Students</link>
-        </div>
-        <div className={styles.card}>
-          <h2>Open Form</h2>
-          <p className={styles.card_words}>Loading...</p>
-          <link href="/forms" className={styles.btn}>Manage Form</link>
-        </div>
-        <table className={styles.course_list}>
-          <thead>
-            <tr>
-              <th>Course ID</th>
-              <th>Course Name</th>
-              <th>class_start</th>
-              <th>class_end</th>
-              <th>Detail</th>
-            </tr>
-          </thead>
-          <tbody className={styles.course_list_body}>
-          </tbody>
-        </table>
-    </main>
-    )
-}
+      <div>
+        <main className={styles.container}>
+          <div className={styles.card}>
+            <h2>Total Courses</h2>
+            <p className={styles.card_words}>{totalCourses}</p>
+            <NavLink to="/formCreate" className={styles.btn}>
+              Manage Courses
+            </NavLink>
+          </div>
+          <div className={styles.card}>
+            <h2>Registered Students</h2>
+            <p className={styles.card_words}>{totalStudents}</p>
+            <a className={styles.btn}>View Students</a>
+          </div>
+          <div className={styles.card}>
+            <h2>Open Form</h2>
+            <p className={styles.card_words}>{totalForms}</p>
+            <NavLink to="/form" className={styles.btn}>
+              Manage Form
+            </NavLink>
+          </div>
 
-export default Dashboard
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <div className={styles.download_file}>
+              <label className={styles.search_lb}>Start date:</label>
+              <input
+                type="date"
+                id="startDate"
+                name="startDate"
+                value={timeslot.startDate}
+                onChange={handleChange}
+                className={styles.search_input}
+              />
+              <label className={styles.search_lb}>End date:</label>
+              <input
+                type="date"
+                id="endDate"
+                name="endDate"
+                value={timeslot.endDate}
+                onChange={handleChange}
+                className={styles.search_input}
+              />
+              <button className={styles.btn} onClick={() => searchStudents()}>
+                Search
+              </button>
+            </div>
+          </form>
+          <div>
+            <button className={styles.btn} onClick={() => downloadResults()}>
+              Export Report
+            </button>
+            <a href="{{url_for({excel_filename})}}" download>
+              Download
+            </a>
+          </div>
+
+          <table className={styles.course_list}>
+            <thead>
+              <tr>
+                <th>Course ID</th>
+                <th>Course Name</th>
+                <th>Detail</th>
+              </tr>
+            </thead>
+            <tbody className={styles.course_list_body}>
+              {selectedProgrammeData.displayedProgrammes.map(
+                (programme, index) => (
+                  <tr key={index}>
+                    <td>{programme.id}</td>
+                    <td>{programme.class_name_eng}</td>
+                    <td>{programme.class_end}</td>
+                  </tr>
+                )
+              )}
+            </tbody>
+          </table>
+        </main>
+      </div>
+    );
+  };
+
+  return <div>{renderDashboard()}</div>;
+};
+
+export default Dashboard;

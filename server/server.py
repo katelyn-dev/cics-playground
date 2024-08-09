84856733aae971f774ff9abfb9d8c050e4ef4e32
unknown <katelynngai@gmail.com>
2024-08-09 14:32:21 -0400

    webapp

--- server/server.py
+++ server/server.py
@@ -1,8 +1,9 @@
 from app import create_app
 from flask_cors import CORS
+import os
 
 app = create_app()
 CORS(app) #do not remove. for cors header 
 
-if __name__ == "__main__": 
-  app.run(host='0.0.0.0', debug=True, port=8080)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 8080))
